e48ad71e4a5a995fea2681a21858e8d8694d49f4
ammakr <aakrambhutta@gmail.com>
2024-02-05 00:39:49 +0500

    add many things

--- nvim/lua/ammar/plugins/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-     "lukas-reineke/indent-blankline.nvim",
-    "christoomey/vim-tmux-navigator",
-    "mbbill/undotree",
-    "tpope/vim-fugitive",
-    "nvim-treesitter/nvim-treesitter-context",
-}
